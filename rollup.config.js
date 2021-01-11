94d8124f0f89537f5888f633cd9ef51af280bbbf
Andres Celis <adwher@outlook.com>
2021-01-10 20:01:28 -0500

    ci: update node version

--- rollup.config.js
+++ rollup.config.js
@@ -4,27 +4,15 @@ export default [
     {
         input: "src/main.ts",
 
-        output: [
-            {
-                name: "cjs",
-                file: "dist/vue-use-firebase.cjs.js",
-                format: "cjs",
-            },
-
-            {
-                name: "esm",
-                file: "dist/vue-use-firebase.esm.js",
-                format: "esm",
-            }
-        ],
+        output: {
+            file: "dist/vue-use-firebase.js",
+            format: "cjs",
+        },
 
         external: [/firebase\/.*/, "vue"],
 
         plugins: [
             esbuild({
-                include: /\.[tj]sx?$/,
-                exclude: /node_modules/,
-                sourceMap: false,
                 minify: true,
                 target: "es2017",
             }),
