d6d3ec877e5f7ea91144628ca73725dcd873fa0d
Andres Celis <adwher@outlook.com>
2021-02-14 20:29:25 -0500

    fix: hot fix v0.4.3-alpha.1

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
 export { createInfiniteScroll, useInfiniteScroll, useCollection } from "./firestore"
-export { useAuth, useSignIn, useSignUp, Authorize } from "./auth"
+export { useAuth, useSignIn, useSignUp, NotLogged, Logged } from "./auth"
 export { useRemoteConfig } from "./remoteConfig"
 export { useLogger } from "./analytics"
