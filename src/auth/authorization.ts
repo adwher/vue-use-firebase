8bedcb8c9b1a34242ecafcb30eb9cebb995feb44
Andres Celis <adwher@outlook.com>
2021-02-21 13:43:54 -0500

    fix: solve watch to authorize components

--- src/auth/authorization.ts
+++ src/auth/authorization.ts
@@ -1,24 +1,28 @@
 import { defineComponent, ref, watch } from "vue"
 import { useAuth } from "./auth"
 
+async function useLogged() {
+    const { user, isLogged } = useAuth()
+
+    const isAuth = ref(await isLogged())
+
+    watch(() => user.uid, function (uid) {	
+        isAuth.value = uid !== "" && uid !== null	
+    })
+
+    return { isAuth, user }
+}
+
 export const Logged = defineComponent({
     async setup(props, { slots }) {
-        const { user, isLogged } = useAuth()
-
-        const isAuth = ref(await isLogged())
-        watch(user, () => isAuth.value = user.uid !== null)
-
+        const { isAuth, user } = await useLogged()
         return () => [isAuth.value ? slots.default?.call(null, user) : null]
     }
 })
 
 export const NotLogged = defineComponent({
     async setup(props, { slots }) {
-        const { user, isLogged } = useAuth()
-
-        const isAuth = ref(await isLogged())
-        watch(user, () => isAuth.value = user.uid !== null)
-
+        const { isAuth } = await useLogged()
         return () => [isAuth.value ? null : slots.default?.call(null)]
     }
 })
